d00ef4cbd797b98fcaa54e7500b9e7d2542363a4
Vincent Bruijn <vebruijn@gmail.com>
2015-10-05 22:43:58 +0200

    refactor animation creation

--- examples/create-animation-gifs.js
+++ examples/create-animation-gifs.js
@@ -1,25 +1,36 @@
 var gd = require('node-gd');
 var fs = require('fs');
 
+// name of end result
 var anim = '../output/logo-anim.gif';
+// source folder
 var srcDir = '../input';
 
-gd.openGif(srcDir + '/node-gd-0.gif', function (error, img) {
-  if (error) throw error;
+// read source folder
+fs.readdir(srcDir, function (error, files) {
+    var gifs = files.filter(function (element) {
+      return element.endsWith('.gif');
+    });
+    gifs.sort();
 
-  img.gifAnimBegin(anim, -1, 0);
-  img.gifAnimAdd(anim, 1, 0, 0, 50, 0, null);
+    // open first frame
+    gd.openGif(srcDir + '/' + gifs.shift(), function (error, img) {
+      if (error) throw error;
 
-  fs.readdir(srcDir, function (error, files) {
-      var gifs = files.filter(function (element) {
-        return element.endsWith('.gif');
-      });
-      gifs.shift();
+      // create first frame
+      img.gifAnimBegin(anim, -1, 0);
+      img.gifAnimAdd(anim, 1, 0, 0, 50, 0, null);
 
+      // recursively add other frames
       addGifToAnimation(gifs, img);
   });
 });
 
+/**
+ * Add frame to animation or, if fileNames is empty, end animation.
+ * fileNames Array
+ * lastFrame gd.Image
+ */
 function addGifToAnimation(fileNames, lastFrame) {
   gd.openGif(srcDir + '/' + fileNames.shift(), function (error, frame) {
     if (error) throw error;
