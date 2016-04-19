afcb69c28123c2ac435b2980f702875d2a987338
Vincent Bruijn <vincent-bruijn@g-star.com>
2016-04-19 17:16:05 +0200

    Update node-gd version, update examples likewise, add some comments

--- examples/create-animation.js
+++ examples/create-animation.js
@@ -10,23 +10,35 @@ var pink = firstFrame.colorAllocate(255, 0, 255);
 
 // create first frame and draw an ellipse
 firstFrame.ellipse(100, -50, 100, 100, pink);
+
 // start animation
 firstFrame.gifAnimBegin(anim, 1, -1);
+// for first frame, the last parameter should be null
+// because there is no former frame
 firstFrame.gifAnimAdd(anim, 0, 0, 0, 5, 1, null);
 
+// create a simple list
 var totalFrames = [];
 for(var i = 0; i < 30; i++) {
   totalFrames.push(i);
 }
 
+// loop list
 totalFrames.forEach(function(i, idx, arr) {
+  // create new frame
   var frame = gd.createSync(200, 200);
   arr[idx] = frame;
+
+  // draw pink ellipse
   frame.ellipse(100, (i * 10 - 40), 100, 100, pink);
+
   var lastFrame = i === 0 ? firstFrame : arr[i - 1];
+
+  // add frame to animation
   frame.gifAnimAdd(anim, 0, 0, 0, 5, 1, lastFrame);
   frame.destroy();
 });
 
+// finish animation
 firstFrame.gifAnimEnd(anim);
 firstFrame.destroy();
--- examples/create-glitch.js
+++ examples/create-glitch.js
@@ -5,16 +5,16 @@ gd.openJpeg('../output/hexagon.jpg', function (error, img) {
   if (error) throw error;
 
   // read it as binary stringat quality 100
-  var imgAsBinaryString = img.jpegPtr(100);
+  var imgAsBinaryString = img.jpegPtr(100, true);
 
   // step through binary data and add some random string
   for (var i = 1024; i < imgAsBinaryString.length; i += (imgAsBinaryString.length / 10)) {
-    imgAsBinaryString = imgAsBinaryString.substring(0, i) + '!@#$%' + imgAsBinaryString.substring(i);
+    imgAsBinaryString.write('!@#$%', i, 5);
   }
 
   try {
     // try to create a new jpeg image pointer from modified binary data
-    var buffer = new Buffer(imgAsBinaryString, 'binary');
+    var buffer = imgAsBinaryString;
     var img = gd.createFromJpegPtr(buffer);
 
     // save it
@@ -22,6 +22,7 @@ gd.openJpeg('../output/hexagon.jpg', function (error, img) {
       if (error) throw error;
     });
   } catch (e) {
+    console.log(e);
     console.log('Error in glitching source image.');
   }
 });
